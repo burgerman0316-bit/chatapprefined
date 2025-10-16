b591b3e58eb01d581e3959b3e4596f553a21252f
burgerman0316-bit <burgerman0316@gmail.com>
2025-10-16 08:44:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const path = require('path');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: { origin: '*' }
 });
 
 // --- SETTINGS & DATA STRUCTURES ---
@@ -550,4 +542,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
+
 
