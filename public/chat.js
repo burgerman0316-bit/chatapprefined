7fd467e278e494dd9cf70b01c8445a9322815d41
burgerman0316-bit <burgerman0316@gmail.com>
2025-10-23 18:18:04 -0500

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -195,18 +195,17 @@ function updatePublicUserList(data) {
         }
         
         li.title = `Click to send private message to ${userDisplayName}`;
-        li.addEventListener("click", () => {
-            // Pre-fill with @username instead of /msg "username"
-            messageInputDiv.innerText = `@${userDisplayName} `;
-            messageInputDiv.focus();
-        
-            // Move cursor to end of input
-            const range = document.createRange();
-            const sel = window.getSelection();
-            range.selectNodeContents(messageInputDiv);
-            range.collapse(false);
-            sel.removeAllRanges();
-            sel.addRange(range);
+        li.addEventListener('click', () => {
+             messageInputDiv.innerText = `/msg \"${userDisplayName}\" `;
+             messageInputDiv.focus();
+             
+             // Move cursor to end of input
+             const range = document.createRange();
+             const sel = window.getSelection();
+             range.selectNodeContents(messageInputDiv);
+             range.collapse(false);
+             sel.removeAllRanges();
+             sel.addRange(range);
         });
         userListEl.appendChild(li);
     });
@@ -764,6 +763,4 @@ socket.on('user count', data => updatePublicUserList(data));
 socket.on('admin_user_map', adminMap => {
     updateAdminManagementList(adminMap);
 });
-
-
 
