d68aa5562f2c0ebea91fc2b900a1eb65643055f7
burgerman0316-bit <burgerman0316@gmail.com>
2025-10-24 13:15:56 -0500

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -57,7 +57,7 @@ let isAdmin = false;
 let userToKick = null; 
 let userGoogleIdToBan = null; 
 let currentChatContext = 'public'; 
-const MAX_CHARS = 10000;
+const MAX_CHARS = 2000;
 const ADMIN_CHAT_ID = 'admin_chat';
 
 // --- Initial Setup ---
@@ -786,3 +786,4 @@ socket.on('user count', data => updatePublicUserList(data));
 socket.on('admin_user_map', adminMap => {
     updateAdminManagementList(adminMap);
 });
+
